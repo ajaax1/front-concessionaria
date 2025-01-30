4b6aff5c8a6399def00340a14a31a09373c76d30
Ruan Higor Silva <ruanhigor123@gmail.com>
2025-01-29 23:38:54 -0300

    modal de usuario

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+const drawer = ref(true);
+</script>
+
+<template>
+  <v-layout>
+    <v-app-bar elevation="0" class="position-absolute border-b" title="Concessionaria">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      </template>
+      <template v-slot:append>
+        <v-btn @click="toggleTheme" icon="mdi-theme-light-dark">
+        </v-btn>
+      </template>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer">
+      <v-list>
+        <v-list-item to="/usuarios" :permanent="true" title="Usuarios"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-layout>
+</template>
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import App from './App.vue'
+import router from './router'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
